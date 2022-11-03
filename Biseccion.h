b85db8733806bbd997709a5b70260a1a6fe727bd
Lopsan Antonio Molina Osorio <lopsan.molina@gmail.com>
2022-11-02 19:16:52 -0600

    Update Biseccion.h

--- Biseccion.h
+++ Biseccion.h
@@ -15,7 +15,6 @@ public:
             ecuation = "1";
         }
         if (ecuation.find("-") != string::npos){
-
             flag = true;
             ecuation.replace(ecuation.find("-"), 1, "");
         }
@@ -67,16 +66,27 @@ public:
         }
         return merged_result;
     }
-
+    float calculate_iteration(float a, float b){
+        return (a+b)/2;
+    }
 };
 
 
-class Biseccion{
+class Ecuation{
 private:
-    int grade, interval1, interval2, *interval;
+    int grade;
+    float *interval, interval1, interval2;
     string *ecuation;
 public:
     BisectionSolveFunction solve = *new BisectionSolveFunction();
+    float getIntervalo1(){
+        return interval1;
+    }
+
+    float getIntervalo2(){
+        return interval2;
+    }
+
     void setGrado() {
         cout << "Introduce el grado del polinomio:" << endl;
         cin >> grade;
@@ -88,7 +98,7 @@ public:
         cin >> interval1;
         cout << "Introduce el final del intervalo: " << endl;
         cin >> interval2;
-        int inter[2] = {interval1, interval2};
+        float inter[2] = {interval1, interval2};
         interval = inter;
     }
 
@@ -103,22 +113,41 @@ public:
         }
     }
 
-    void sustituir(){
+    bool isValidFunction(map<string, float> merged_result){
+        float first_result, second_result;
+        for (map<string, float>::iterator it2 = merged_result.begin(); it2 != merged_result.end(); ++it2){
+            cout << it2->first << ":" << it2->second<< endl;
+            if (first_result == 0.0){
+                first_result = it2->second;
+            } else {
+                second_result = it2->second;
+            }
+        }
+        return (first_result > 0 and second_result < 0) or (first_result < 0 and second_result > 0);
+    }
+
+    map<string, float> sustituir(float *_interval){
         string cases[3] = {"cos", "sen", "tan"};
         map<string, map<string, float>> results;
         map<string, float> merged_result;
         float result;
+        float first_result = 0.0, second_result = 0.0;
+        if (_interval[0] == 9999){
+            _interval = interval;
+        }
         for (int i = 0; i < grade + 1; i++) {
             try{
                 result = stof(ecuation[i]);
-                for (int i = 0; i < sizeof(interval)/sizeof(interval[0]); i++){
-                    results[to_string(grade)][to_string(interval[i])] = result;
+                cout << "esto no debio de pasar" << endl;
+                cout << result << endl;
+                for (int i = 0; i < sizeof(&_interval)/sizeof(_interval[0]); i++){
+                    results[to_string(grade)][to_string(_interval[i])] += result;
                 }
             } catch (const std::invalid_argument) {
                 float result;
                 for (int j = 0; j < 3; j++){
                     string type_ecuation;
-                    for (int k = 0; k < sizeof(interval)/sizeof(interval[0]); k++) {
+                    for (int k = 0; k < sizeof(&_interval)/sizeof(_interval[0]); k++) {
                         if (ecuation[i].find(cases[j]) != string::npos) {
                             type_ecuation = cases[j];
                         } else if (!(ecuation[i].find(cases[j]) != string::npos) and ecuation[i].find("x") != string::npos) {
@@ -126,8 +155,8 @@ public:
                         }
                         string new_ecuation = solve.reduce_ecuation(ecuation[i], type_ecuation);
                         try {
-                            result = solve.get_solved_function(type_ecuation,new_ecuation,interval[k],i);
-                            results[to_string(grade)][to_string(interval[k])] = result;
+                            result = solve.get_solved_function(type_ecuation,new_ecuation,_interval[k],i);
+                            results[to_string(grade)][to_string(_interval[k])] += result;
                         } catch (std::invalid_argument) {
                             //
                         }
@@ -136,10 +165,66 @@ public:
             }
             merged_result = solve.merge_results(results);
         }
-        for (map<string, float>::iterator it2 = merged_result.begin(); it2 != merged_result.end(); ++it2){
-            cout << it2->first << ":" << it2->second<< endl;
+        return merged_result;
+    }
+    float getIteration(){
+        float iteration;
+        iteration = solve.calculate_iteration(interval1, interval2);
+        return iteration;
+    }
+};
+
+
+class Bisection {
+public:
+    Ecuation ecu = *new Ecuation();
+
+    void main() {
+        map<string, float> result, result2;
+        ecu.setGrado();
+        ecu.generar_ecuacion();
+        ecu.fillEcuacion();
+        ecu.setIntervalo();
+        float _inter[2] = {9999, 9999};
+
+        result = ecu.sustituir(_inter);
+        if (ecu.isValidFunction(result)) {
+            float first_result, second_result;
+            for (map<string, float>::iterator it2 = result.begin(); it2 != result.end(); ++it2){
+                cout << it2->first << ":" << it2->second<< endl;
+                if (first_result == 0.0){
+                    first_result = it2->second;
+                } else {
+                    second_result = it2->second;
+                }
+            }
+            float efe_de_equis = first_result * second_result;
+            float error = 1;
+            float inter[2] = {ecu.getIntervalo1(), ecu.getIntervalo2()};
+            float iteration = ecu.solve.calculate_iteration(inter[0], inter[1]);
+            while (error > pow(10, -5)){
+                if (efe_de_equis > 0){
+                    inter[0] = iteration;
+                } else {
+                    inter[1] = iteration;
+                }
+                result = ecu.sustituir(inter);
+                iteration = ecu.solve.calculate_iteration(inter[0], inter[1]);
+                for (map<string, float>::iterator it2 = result.begin(); it2 != result.end(); ++it2){
+                    cout << it2->first << ":" << it2->second<< endl;
+                    if (first_result == 0.0){
+                        first_result = it2->second;
+                    } else {
+                        second_result = it2->second;
+                    }
+                }
+                error = 1;
+            }
+        } else {
+            cout << "no es valido" << endl;
         }
     }
+
 };
 
 
@@ -147,10 +232,11 @@ public:
 int main() {
     int grado;
     string *ecuacion;
-    Biseccion bi = Biseccion();
+    Ecuation bi = Ecuation();
     bi.setGrado();
     bi.generar_ecuacion();
     bi.fillEcuacion();
     bi.setIntervalo();
-    bi.sustituir();
+    float _inter[2] = {9999, 9999};
+    bi.sustituir(_inter);
 };
